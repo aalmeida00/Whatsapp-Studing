6a37e3a600f0e55ef9eca721f72f8ac073115144
aalmeida00 <andre-silva78@hotmail.com>
2020-08-22 02:02:31 -0300

    :boom: First commit, create-react-app + first cleanup on some files and add some configurations :no_bicycles:

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDTmpTW9j3X11pdkU4aJPHXnlTp_wVUuWA",
+  authDomain: "whatsapp-clone-c1349.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-c1349.firebaseio.com",
+  projectId: "whatsapp-clone-c1349",
+  storageBucket: "whatsapp-clone-c1349.appspot.com",
+  messagingSenderId: "983252944460",
+  appId: "1:983252944460:web:5a0334ca321eef5ac156e9",
+};
